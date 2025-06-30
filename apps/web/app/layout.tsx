3073d7f65a76021a3dc7d39a6e8cbe79573762a0
jainam <pateljainams122@gmail.com>
2025-06-30 21:46:35 +0530

    added users table

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -25,7 +25,7 @@ export const metadata: Metadata = {
   siteName:"Podler",
   images:[
     {
-      url:'https://podly-recordings.s3.eu-north-1.amazonaws.com/images/og-image.png',
+      url:'/og-image1.png',
       width: 1200,
         height: 630,
         alt: "OG IMAGE",
