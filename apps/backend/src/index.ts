8b8053fbad5caa1eb490454c80c0af43fdf82ce6
jainam <pateljainams122@gmail.com>
2025-07-07 09:56:53 +0530

    added pings

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -9,7 +9,7 @@ const server = http.createServer(app)
 
 const io = new Server(server,{
     cors:{
-        origin:"*",
+        origin:['https://podler.space','http://localhost:3000'],
         methods:["GET","POST"]
     }
 })
